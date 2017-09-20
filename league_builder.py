cc8485a7fe7d414f1aa18d61c00249060452eb96
Enzie Riddle <enzieriddle@gmail.com>
2017-09-20 16:43:52 -0500

    Returns and Variable Cleanup
    Swapped locations of `def read()` and `def each_player(players)`. Changed new `each_player` variable name to `the_player`. Removed `if` block for now. Calling `read()` and `each_player()` for testing.

--- league_builder.py
+++ league_builder.py
@@ -1,25 +1,24 @@
 import csv
 
-# Create team lists
-Dragons = []
-Sharks = []
-Raptors = []
-def each_player(players):
-    experienced = []
-    new = []
-    for each_player in players:
-        if each_player['Soccer Experience'] == 'YES':
-            experienced.append(each_player)
-        else:
-            new.append(each_player)
-    print(experienced)
-    print(new)
 # Open and read file.
 def read():
     with open('soccer_players.csv', newline='') as csvfile:
         player_reader = csv.DictReader(csvfile, delimiter=',')
         players = list(player_reader)
+        return players
+
+# Create team lists
+def each_player(players):
+    experienced = []
+    new = []
+    for the_player in players:
+        if each_player['Soccer Experience'] == 'YES':
+            experienced.append(the_player)
+        else:
+            new.append(the_player)
+    print(experienced)
+    print(new)
 
 # Put all logic and function calls inside block
-if __name__ == "__main__":
+
 
