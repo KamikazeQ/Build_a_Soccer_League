15159cffeb3302eff3aaaffc1f37350e24d47213
Enzie Riddle <enzieriddle@gmail.com>
2017-09-20 16:19:24 -0500

    Typos
    Corrected typos

--- league_builder.py
+++ league_builder.py
@@ -1,24 +1,25 @@
 import csv
 
+# Create team lists
+Dragons = []
+Sharks = []
+Raptors = []
+def each_player(players):
+    experienced = []
+    new = []
+    for each_player in players:
+        if each_player['Soccer Experience'] == 'YES':
+            experienced.append(each_player)
+        else:
+            new.append(each_player)
+    print(experienced)
+    print(new)
 # Open and read file.
 def read():
-    with open('path\to\soccer_players.csv', newline='') as cvsfile:
-        player_reader = csv.DictReader(csvfile, delimeter=',')
+    with open('soccer_players.csv', newline='') as csvfile:
+        player_reader = csv.DictReader(csvfile, delimiter=',')
         players = list(player_reader)
 
 # Put all logic and function calls inside block
 if __name__ == "__main__":
-    # Create team lists
-    Dragons = []
-    Sharks = []
-    Raptors = []
-    def each_player(players):
-        experienced = []
-        new = []
-        for each_player in players:
-            if each_player['Soccer Experience'] == 'YES':
-                experienced.append(each_player)
-            else:
-                new.append(each_player)
-        print(experienced)
-        print(new)
+
