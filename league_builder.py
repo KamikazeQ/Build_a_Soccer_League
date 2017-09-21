96a7e4cf55745593f862296a830ad96ca6bd5843
Enzie Riddle <enzieriddle@gmail.com>
2017-09-21 17:38:43 -0500

    Update league_builder.py

--- league_builder.py
+++ league_builder.py
@@ -6,10 +6,10 @@ def read():
         player_reader = csv.DictReader(csvfile, delimiter=',')
         players = list(player_reader)
         return players
-
-player_list_from_csv = read()
     
-# Create team lists
+player_list_from_csv = read()
+
+# Find experienced players vs. not experienced players
 def find_experienced(player_list_from_csv):
     experienced = []
     new = []
@@ -18,23 +18,40 @@ def find_experienced(player_list_from_csv):
             experienced.append(the_player)
         else:
             new.append(the_player)
-    return experienced
-    return new
+    return experienced, new
 
-# Put all logic and function calls inside block
-if __name__ == '__main__':
-    Dragons = []
-    Sharks = []
-    Monsters = []
-    while True:
-        find_experienced(player_list_from_csv)
-        if len(experienced) in Dragons < 4:
-            Dragons.append(experienced)
-        elif len(experienced) in Sharks < 4:
-            Sharks.append(experienced)
-        else:
-            Monsters.append(experienced)
-    print(Dragons)
-    print(Sharks)
-    print(Monsters)
+experienced_and_new_players = experienced, new
 
+# Distributes experienced and new players
+def players_per_team(experienced_and_new_players):
+        Dragons = []
+        Sharks = []
+        Monsters = []
+        
+        for player in experienced:
+            if len(experienced) in Dragons < 4:
+                Dragons.append(experienced)
+            elif len(experienced) in Sharks < 4:
+                Sharks.append(experienced)
+            else:
+                Monsters.append(experienced)
+        for player in new:
+            if len(new) in Dragons < 4:
+                Dragons.append(new)
+            if len(new) in Sharks < 4:
+                Sharks.append(new)
+            else:
+                Monsters.append(new)
+        return Dragons, Sharks, Monsters
+        print(Dragons)
+        print(Sharks)
+        print(Monsters)
+        
+if __name__ == '__main__':        
+    read()
+    find_experienced(player_list_from_csv)
+    players_per_team(experienced_and_new_players)
+
+# Put all logic and function calls inside block    
+
+        
