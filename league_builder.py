27e0b7dd66de154f7c89423a0fd5dc4a7efb62d7
Enzie Riddle <enzieriddle@gmail.com>
2017-09-21 16:49:32 -0500

    Cleanup and Experienced Players Logic
    Added player list variable from read() function, cleaned up find_experience(player_list_from_csv) function, added logic for adding experienced players to teams.

--- league_builder.py
+++ league_builder.py
@@ -7,20 +7,34 @@ def read():
         players = list(player_reader)
         return players
 
+player_list_from_csv = read()
+    
 # Create team lists
-def find_experienced(players):
+def find_experienced(player_list_from_csv):
     experienced = []
     new = []
-    for the_player in players:
-        if each_player['Soccer Experience'] == 'YES':
+    for the_player in player_list_from_csv:
+        if the_player['Soccer Experience'] == 'YES':
             experienced.append(the_player)
         else:
             new.append(the_player)
-    print(experienced)
-    print(new)
+    return experienced
+    return new
 
-read()
-find_experienced(players)
 # Put all logic and function calls inside block
-
+if __name__ == '__main__':
+    Dragons = []
+    Sharks = []
+    Monsters = []
+    while True:
+        find_experienced(player_list_from_csv)
+        if len(experienced) in Dragons < 4:
+            Dragons.append(experienced)
+        elif len(experienced) in Sharks < 4:
+            Sharks.append(experienced)
+        else:
+            Monsters.append(experienced)
+    print(Dragons)
+    print(Sharks)
+    print(Monsters)
 
